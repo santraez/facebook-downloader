5c8a25e771d6a8b8d09b0e7e6878fa545ad1c753
santraez <santraez@gmail.com>
2023-06-21 10:23:16 -0500

    TEST 006

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -7,17 +7,17 @@ export async function GET(request) {
   const { searchParams } = new URL(request.url)
   const url = searchParams.get("url")
   const newUrl = url.replace("www", "m")
-  const browser = await puppeteer.launch({ headless: "new" })
-  const page = await browser.newPage()
-  await page.goto(newUrl)
-  await delay(2000)
-  const element = await page.waitForSelector("div[data-sigil='m-video-play-button playInlineVideo']")
-  await element.click()
-  await delay(3000)
-  const src = await page.evaluate(() => {
-    const video = document.querySelector("div[data-sigil='inlineVideo'] > video")
-    return video.src
-  })
-  await browser.close()
+  // const browser = await puppeteer.launch({ headless: "new" })
+  // const page = await browser.newPage()
+  // await page.goto(newUrl)
+  // await delay(2000)
+  // const element = await page.waitForSelector("div[data-sigil='m-video-play-button playInlineVideo']")
+  // await element.click()
+  // await delay(3000)
+  // const src = await page.evaluate(() => {
+  //   const video = document.querySelector("div[data-sigil='inlineVideo'] > video")
+  //   return video.src
+  // })
+  // await browser.close()
   return NextResponse.json({ videoLink: newUrl })
 }
